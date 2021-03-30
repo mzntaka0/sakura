0cd769729dac83fdc2b2bfc1f4d797d5d7ce4fd0
JeanMaximilienCadic <info@cadic.jp>
2021-03-30 12:23:20 +0900

    update setup

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@ docker pull zakuroai/sakura
 docker run --rm \
   --gpus all \
   --shm-size=70g \
-  -v $(pwd)/data:/workspace/data -v $(pwd)/demo.py:/workspace/demo.py -it zakuroai/sakura bash # python demo.py
+  -v $(pwd)/data:/workspace/data -v $(pwd)/demo.py:/workspace/demo.py -it zakuroai/sakura python demo.py
