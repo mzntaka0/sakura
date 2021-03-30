e3f597c53998e292d63af6f2be768c2fbf79abdf
JeanMaximilienCadic <info@cadic.jp>
2021-03-30 12:22:49 +0900

    update setup

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM  pytorch/pytorch
-RUN pip uninstall sakura
-
-WORKDIR /workspace
-
-RUN apt update -y && apt install git -y
-RUN pip install git+https://github.com/zakuro-ai/sakura.git
--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-docker build . -t zakuro/sakura
+docker pull zakuroai/sakura
 docker run --rm \
   --gpus all \
   --shm-size=70g \
-  -v $(pwd)/data:/workspace/data -v $(pwd)/demo.py:/workspace/demo.py -it zakuro/sakura bash # python demo.py
+  -v $(pwd)/data:/workspace/data -v $(pwd)/demo.py:/workspace/demo.py -it zakuroai/sakura bash # python demo.py
